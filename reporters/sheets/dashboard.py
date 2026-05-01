de2d668852be08b64eb94fba39eaa836db5a567f
Courage-1984 <dieter.dieter.no1@gmail.com>
2026-05-01 18:37:51 +0200

    before another refactor

---
+++ reporters/sheets/dashboard.py
@@ -0,0 +1,3 @@
+from reporters.sheets.tables_core import _style_dashboard
+
+__all__ = ["_style_dashboard"]
---
+++ reporters/sheets/tables_core.py
@@ -0,0 +1,3 @@
+from reporters.sheets.tables_impl import adjust_sheet_format, apply_tab_hyperlinks, _set_freeze_panes_safe
+
+__all__ = ['adjust_sheet_format', 'apply_tab_hyperlinks', '_set_freeze_panes_safe']
